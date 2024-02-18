bfba7f4e3f9295ba294f8dcad4d35e0aff8b3e26
gowindcode <gowindcan@gmail.com>
2024-02-18 16:16:29 +0530

    port update in index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const billRoute = require("../backend/routes/billRoute");
 const homeRoute = require("../backend/routes/homeRoute");
 
 const app = express();
-const PORT = process.env.PORT || PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
